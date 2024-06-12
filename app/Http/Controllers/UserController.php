1ad6407d8349f36d8d26241cd8a09d6745cc6cdf
Lucas Lacerda <lucaslacerdaandrey@gmail.com>
2024-06-11 21:30:26 -0300

    adicionado endpoint de listar, atualizar e deletar o usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+
+    public function index(): JsonResponse
+    {
+
+        $users = User::all();
+
+        return response()->json($users->toArray(), Response::HTTP_OK);
+
+    }
+
+    public function show(User $user): JsonResponse
+    {
+
+        //return User::query()->find($user);
+        //return User::query()->findOrFail($user);
+        return response()->json($user->toArray(), Response::HTTP_OK);
+    }
+
     public function store(Request $request): JsonResponse
     {
         $request->validate(
@@ -21,8 +39,32 @@ class UserController extends Controller
             ]
         );
 
-       $user = User::query()->create($request->all());
+        $user = User::query()->create($request->all());
 
-       return response()->json($user->toArray(), Response::HTTP_CREATED);
+        return response()->json($user->toArray(), Response::HTTP_CREATED);
+    }
+
+    public function update(User $user, Request $request): JsonResponse
+    {
+        $request->validate(
+            [
+                'first_name' => ['nullable', 'string', 'max:255'],
+                'last_name' => ['nullable', 'string', 'max:255'],
+                'document_cpf' => ['nullable', 'string', 'size:11', 'unique:users'],
+                'email' => ['nullable', 'string', 'email:rfc,filter', 'max:255', 'unique:users'],
+                'password' => ['nullable', 'string', 'min:8'],
+            ]
+        );
+
+        $user->update($request->all());
+
+        return response()->json($user->refresh()->toArray(), Response::HTTP_OK);
+    }
+
+    public function delete(User $user): \Illuminate\Http\Response
+    {
+        $user->delete();
+
+        return response()->noContent();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::post('/users', [UserController::class, 'store']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/{user}', [UserController::class, 'show']);
+
+Route::patch('/users/{user}', [UserController::class, 'update']);
+
+Route::delete('/users/{user}', [UserController::class, 'delete']);
